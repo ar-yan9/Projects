79481c763e5d8aeb560cbf79e9ce31a8a7d2dcca
ar-yan9 <aryankumar6360@gmail.com>
2026-05-17 15:43:51 +0530

    Add Web Scraping project

---
+++ Web Scraping/main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w", encoding="utf-8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+
+print("Top 100 Movies saved to movies.txt!")
+for movie in movies:
+    print(movie)
