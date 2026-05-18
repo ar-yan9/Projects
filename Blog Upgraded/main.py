4c36539a7095d9cf86c063d850f5d8fd96befa58
ar-yan9 <aryankumar6360@gmail.com>
2026-05-18 13:34:26 +0530

    Add Blog Upgraded project

---
+++ Blog Upgraded/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    blog_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", all_posts=all_posts[:10])
+
+@app.route("/post/<int:index>")
+def get_post(index):
+    blog_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    requested_post = all_posts[index - 1]
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
